ac0837d6bf44e41d121ef4a6c6dab4c996de4ed3
Bhoomik shetty <shettybhoomik@gmail.com>
2026-01-22 02:05:43 +0530

    ws project

--- mongoose.js
+++ mongoose.js
@@ -1,3 +1,98 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import { type } from "os";
+import { required } from "zod/mini";
 
-mongoose.connect('')
+// const db = await mongoose.connect('mongodb://localhost:27017/websocket/?directConnection=true')
+await mongoose.connect('mongodb://localhost:27017/websocket',).then(() => console.log("Connected to Mongodb"))
+    .catch((err) => console.error('Error conneting to DB: ', err))
+
+const dbInstance = mongoose.connection;
+dbInstance.once('open', () => console.log("Open DB instance"))
+dbInstance.once('connection', () => console.log("Open DB instance connection"))
+dbInstance.once('close', () => console.log("Open DB instance connection"))
+
+
+const UserSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    role: {
+        type: String,
+        enum: ['teacher', 'student'],
+        required: true
+    }
+}, { timestamps: true })
+const User = mongoose.model('user', UserSchema)
+
+// await User.deleteMany()
+
+// console.log(await User.create({
+//     name: "BHOOMIK",
+//     email: 'shettybhoomik@gmail.ce',
+//     password: 'pass',
+//     role: 'teacher',
+// }))
+
+const ClassSchema = new Schema(
+    {
+        className: { type: String, required: true, unique: true },
+        teacherId: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        studentIds: [{
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }]
+    }
+)
+
+const Class = mongoose.model('class', ClassSchema)
+// console.log(db)
+
+const AttendanceSchema = new Schema({
+    classId: {
+        type: Schema.Types.ObjectId,
+        unique: true,
+        ref: 'class'
+    },
+    studentId: {
+        type: Schema.Types.ObjectId,
+        unique: true,
+        ref: 'user'
+    },
+    status: {
+        type: String,
+        enum: ['present', 'absent'],
+    }
+})
+const Attendance = mongoose.model('attendance', AttendanceSchema)
+
+const newPromise = new Promise((resolve, reject) => {
+    Math.random() < 0.5 ? resolve({
+        "message": "Hey resoleve ho gaya"
+    }) : reject({
+        "message": "OOPS! reject ho gaya"
+    })
+})
+
+
+
+fetch("https://api.genderize.io?name=luc").then((res) => console.log(res.body)).catch((err) => console.log("API ERROR", err))
+
+
+// newPromise.then((res) => console.log("Promise resolverd", res))
+// console.log("END")
+
+// var test = '5' + 5 - 5
+// test = undefined + 8
+// test = 5 + undefined
+// test = undefined + undefined
+// test = undefined - undefined
+// test = undefined + undefined
+
+// console.log(test)
+// console.log(typeof (test))
+
+
+export { User, Class, Attendance }
--- websocket.js
+++ websocket.js
@@ -6,13 +6,19 @@ import jsonwebtoken from 'jsonwebtoken'
 import bodyParser from 'body-parser'
 
 import dotenv from 'dotenv'
-import { length, z } from 'zod'
+import { email, length, success, z } from 'zod'
+
+import { User, Class, Attendance } from './mongoose.js'
+import { error } from 'console'
+import { da } from 'zod/locales'
+
+import urll from 'url';
 
 dotenv.config()
 const app = express()
 const router = express.Router()
 
-router.post('/signup', verify)
+router.post('/signup', verifyToken)
 
 const server = createServer(app)
 const wss = new WebSocketServer({ server })
@@ -21,35 +27,136 @@ app.use(express.json())
 // app.use(verifyToken)
 app.use(bodyParser.json());
 
-const activeSession = {
-    classId: "c101", // current active class
-    startedAt: "2025-03-11T10:00:00.000Z", // ISO string
-    attendance: {
-        "s100": "present",
-        "s101": "absent"
-        // studentId: status
-    }
-};
+var activeSession = {};
 
+var sockets = [];
 
 //wss is the main socket which listens for 
 // any new client connection requests
-wss.on('connection', (newSocketInstance) => { // This "newSocketInstance" is the new instance for a new client
-    console.log("msg")
+wss.on('connection', (ws, req) => { // This "newSocketInstance" is the new instance for a new client
+    const token = urll.parse(req.url).query.split("=")[1]
+    const payload = decodeAccessToken(token)
 
-    newSocketInstance.on('message', (data, isBinary) => {
-        newSocketInstance.send("returning the input " + data);
-        console.log("OPEN")
+    if (!payload) {
+        res.send(JSON.stringify({
+            "event": "ERROR",
+            "data": {
+                "message": "Unauthorized or invalid token"
+            }
+        }))
+        ws.close()
+    }
+
+    ws.user = {
+        'userId': payload._id,
+        'role': payload.role,
+    }
+
+    ws.on('message', async (data, isBinary) => {
+        console.error(ws.user)
+
+        const eventData = JSON.parse(data.toString())
+        if (ws.user.role === 'teacher' && eventData.event === 'ATTENDANCE_MARKED') {
+
+            activeSession.attendance[eventData.data.studentId] = eventData.data.status;
+            wss.clients.forEach((client) => {
+                if (client.user.role === 'student') {
+
+                    client.send(JSON.stringify(eventData));
+                }
+            })
+        }
+
+
+        if (ws.user.role === 'teacher' && eventData.event === 'TODAY_SUMMARY') {
+            var list = Object.entries(activeSession.attendance)
+            var present = Array.from(list)
+                .filter(([key, value]) => value === 'present')
+                .length;
+
+            wss.clients.forEach((client) => {
+                if (client.user.role === 'student') {
+                    client.send(JSON.stringify({
+                        "event": "TODAY_SUMMARY",
+                        "data": {
+                            "present": present,
+                            "absent": list.length - present,
+                            "total": list.length
+                        }
+                    }));
+                }
+            })
+        }
+
+
+        if (ws.user.role === 'student' && eventData.event === 'MY_ATTENDANCE') {
+            ws.send(JSON.stringify({
+                "event": "MY_ATTENDANCE",
+                "data": {
+                    "status": activeSession.attendance[ws.user.userId] ?? null
+                }
+            }))
+        }
+
+        if (ws.user.role === 'teacher' && eventData.event === 'DONE') {
+
+            var user = await User.find({ role: 'student' })
+            var _class = await Class.findById(activeSession.classId).populate(['studentIds'])
+
+            _class.studentIds.forEach((student) => {
+                if (activeSession.attendance[student._id.toString()] == null) {
+                    activeSession.attendance[student._id.toString()] = 'absent'
+                }
+            });
+
+            var list = Object.entries(activeSession.attendance)
+            var newList = list.map(async (student) => await Attendance.create({
+                classId: activeSession.classId,
+                studentId: student[0],
+                status: student[1],
+            }));
+
+
+            var list = Object.entries(activeSession.attendance)
+            var present = Array.from(list)
+                .filter(([key, value]) => value === 'present')
+                .length;
+
+            wss.clients.forEach((client) => {
+                if (client.user.role === 'student') {
+                    client.send(JSON.stringify({
+                        "event": "DONE",
+                        "data": {
+                            "present": present,
+                            "absent": list.length - present,
+                            "total": list.length
+                        }
+                    }));
+                }
+            })
+            activeSession = {};
+        }
     })
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 server.listen(3000, () => {
     console.log("Hello there!")
-})
-
-app.get('/gett', (req, res) => {
-    console.log("Hey")
-    res.send("gettt")
 })
 
 function verifyToken(token) {
@@ -61,44 +168,424 @@ function verifyToken(token) {
     }
 }
 
-function generateAccessToken(data) {
-    return jsonwebtoken.sign(data, process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY)
+function generateAccessToken(payload) {
+    return jsonwebtoken.sign(payload, process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY)
 }
 
-function generateRefreshToken(data) {
-    return jsonwebtoken.sign(data, process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY)
+function generateRefreshToken(payload) {
+    return jsonwebtoken.sign(payload, process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY)
 }
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     try {
-        const token = req.headers.authorization
-        const { username, password } = req.body
 
-        if (username !== null || password !== null) {
-            generateAccessToken({});
-            generateRefreshToken();
+        var logInData = LoginSchema.parse(req.body)
+
+        const user = await User.findOne({ email: logInData.email })
+        if (user) {
+            if (logInData.password !== user.password) return res.status(400).json({
+                "success": false,
+                "error": "Invalid email or password"
+            })
+
+            const jwt = generateAccessToken({ _id: user._id, email: user.email, role: user.role })
+            res.json({
+                "success": true,
+                "data": {
+                    "token": jwt
+                }
+            })
         }
-        console.log(user)
+        else {
+            return res.status(400).json({
+                "success": false,
+                "error": "Invalid email or password"
+            })
+        }
+
     } catch (error) {
         console.error(error)
         res.status(500).send()
     }
 })
 
-app.post('/signup', (req, res) => {
-
-    let signUpData;
+// TODO: all the cases of validation
+app.post('/signup', async (req, res) => {
     try {
-        signUpData = SignupSchema.parse(req.body)
-        const token = jsonwebtoken.sign(req.body, process.env.JWT_ACCESS_TOKEN_PRIVATE_KEY)
-        console.log(token)
-        res.json({
-            "token": token
-        }).send()
+        var signUpData = SignupSchema.parse(req.body)
+
+        const user = await User.create({
+            name: signUpData.name,
+            email: signUpData.email,
+            password: signUpData.password,
+            role: signUpData.role
+        })
+
+        res.status(201).send({
+            success: true,
+            data: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        });
+    } catch (error) {
+
+        if (error.code === 11000) {
+            return res.status(400).json({ success: false, error: 'Email already exists' });
+        }
+        console.error(error)
+        res.status(500).send()
+    }
+})
+
+function decodeAccessToken(token) {
+    return jsonwebtoken.decode(token, process.env.JWT_ACCESS_TOKEN_PRIVATE_KEY)
+}
+
+app.get('/auth/me', async (req, res) => {
+    const token = req.headers.authorization;
+    const payload = decodeAccessToken(token)
+
+    if (!payload) {
+        return res.status(403).json({
+            success: false,
+            error: "Unauthorized access",
+        })
+    }
+
+    const user = await User.findById(payload._id)
+    if (user) {
+        return res.status(200).json({
+            success: true,
+            data: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        })
+    }
+
+    return res.status(403).json({ success: false, error: "Unauthorized access" })
+})
+
+app.post('/class', async (req, res) => {
+    try {
+
+        const token = req.headers.authorization;
+        const payload = decodeAccessToken(token)
+
+        if (!payload) {
+            return res.status(403).json({
+                success: false,
+                error: "Unauthorized access",
+            })
+        }
+
+
+        if (payload.role === 'teacher') {
+
+            var classData = ClassSchema.parse(req.body)
+
+            var _class = await Class.create({
+                className: classData.className,
+                teacherId: payload._id,
+                studentIds: []
+            })
+
+            res.status(201).json({
+                "success": true,
+                "data": {
+                    "_id": _class._id,
+                    "className": _class.className,
+                    "teacherId": _class.teacherId,
+                    "studentIds": []
+                }
+            })
+        } else {
+            res.status(403).json({
+                success: false,
+                error: "Unauthorized access"
+            })
+        }
+    } catch (error) {
+        console.error(error)
+        if (error.code === 11000) {
+            return res.status(400).json({
+                success: false
+            })
+        }
+        res.status(500).json()
+    }
+})
+
+app.post('/class/:classId/add-student', async (req, res) => {
+    try {
+        const classId = req.params.classId;
+
+        const token = req.headers.authorization;
+        const payload = decodeAccessToken(token)
+
+        if (!payload) {
+            return res.status(403).json({
+                success: false,
+                error: "Unauthorized access",
+            })
+        }
+
+        const studentId = req.body.studentId;
+        console.log(studentId)
+
+        var _class = await Class.findById(classId)
+        // .populate(['teacherId', 'studentIds'])
+
+        if (_class) {
+            if (_class.teacherId._id.toString() === payload._id) {
+                var studentIds = []
+                studentIds = _class.studentIds
+
+                const user = await User.findById(studentId);
+                if (!user) return res.status(400).json({
+                    success: false,
+                    error: `No student with id ${studentId} found`
+                })
+                if (studentIds.map((student) => student._id.toString()).includes(studentId)) {
+                    return res.status(201).json({
+                        success: false, error: "student already added to the class"
+                    })
+                }
+                studentIds.push(studentId)
+                await Class.findByIdAndUpdate(classId, { studentIds: studentIds })
+
+                return res.status(201).json({
+                    "success": true,
+                    "data": {
+                        "_id": _class._id,
+                        "className": _class.name,
+                        "teacherId": _class.teacherId._id.toString(),
+                        "studentIds": studentIds
+                    }
+                })
+            }
+            else {
+                res.status(403).json({ success: false, error: "Unauthorized access" })
+            }
+
+        }
+        else {
+            res.status(404).json({ success: false, error: "Class not found" })
+        }
 
     } catch (error) {
         console.error(error)
-        res.status(500).send(signUpData[0].message)
+        res.status(500).json()
+    }
+
+})
+
+
+
+app.get('/class/:classId', async (req, res) => {
+    const classId = req.params.classId;
+
+    const token = req.headers.authorization;
+    const payload = decodeAccessToken(token)
+
+    if (!payload) {
+        return res.status(403).json({
+            success: false,
+            error: "Unauthorized access",
+        })
+    }
+
+    var _class = await Class.findById(classId).populate(['teacherId', 'studentIds'])
+
+    if (_class.teacherId._id.toString() === payload._id || _class.studentIds.map((student) => student._id.toString()).includes(payload._id)) {
+
+        var students = _class.studentIds.map((student) => ({
+            _id: student._id,
+            name: student.name,
+            email: student.email
+        }));
+        return res.status(200).json({
+            success: true,
+            data: {
+                _id: _class._id,
+                className: _class.name,
+                teacherId: _class.teacherId._id.toString(),
+                students: students
+            }
+        })
+    } else {
+        res.status(400).json(
+            {
+                success: false,
+                error: "No such record found"
+            }
+        )
+    }
+})
+
+app.get('/students', async (req, res) => {
+
+    const token = req.headers.authorization;
+    const payload = decodeAccessToken(token)
+
+    if (!payload) {
+        return res.status(403).json({
+            success: false,
+            error: "Unauthorized access",
+        })
+    }
+
+    if (payload.role === 'teacher') {
+        var users = await User.find({ role: 'student' })
+        var students = users.map((user) => ({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        }));
+
+        res.status(200).json({ success: true, data: students })
+    }
+    else {
+        res.status(400).json({
+            success: false, error: "Unauthorized access"
+        })
+    }
+})
+
+// TODO: make the classId and studentId required in Atterndance Schema, 
+// Refer
+// [
+//   {
+//     _id: new ObjectId('6971300d86800c889b823605'),
+//     classId: new ObjectId('696ff2ffb12f7eaf53a1e02f'),
+//     studentId: new ObjectId('696febed114eebbc44012e17'),
+//     status: 'present',
+//     __v: 0
+//   },
+//   {
+//     _id: new ObjectId('697131f1f95c41eff3b2f153'),
+//     classId: new ObjectId('696ff2ffb12f7eaf53a1e02f'),
+//     studentId: new ObjectId('696febed114eebbc44012e17'),
+//     status: 'absent',
+//     __v: 0
+//   }
+// ]
+
+
+// TODO: CHECK AFTER WS IMPL
+app.get('/class/:classId/my-attendance', async (req, res) => {
+    const token = req.headers.authorization;
+    const payload = decodeAccessToken(token)
+
+    // res.send(activeSession)
+    if (!payload) {
+        return res.status(403).json({
+            success: false,
+            error: "Unauthorized access",
+        })
+    }
+
+    const classId = req.params.classId
+    var _class = await Class.findById(classId).populate(['studentIds'])
+
+    if (payload.role === 'student') {
+
+        if (_class.studentIds.map((student) => student._id.toString()).includes(payload._id)) {
+            var attendance = await Attendance.find({ classId: classId, studentId: payload._id });
+
+            console.log(attendance)
+            if (attendance.length === 0) {
+                return res.status(200).json({
+                    success: true,
+                    data: {
+                        classId: classId,
+                        status: null
+                    }
+                })
+            }
+            else {
+                return res.status(200).json({
+                    success: true,
+                    data: {
+                        classId: classId,
+                        status: attendance[0].status,
+                    }
+                })
+            }
+        } else {
+            return res.status(400).json({
+                success: false,
+                error: `Student ${payload._id} is not enrolled in class ${classId}`
+            })
+        }
+    }
+    else {
+        return res.status(400).json({
+            success: false,
+            error: `Unauthorized access`
+        })
+    }
+
+})
+
+
+app.post('/attendance/start', async (req, res) => {
+    try {
+        console.log(activeSession.classId)
+        if (activeSession.classId !== undefined) {
+            return res.status(400).json({
+                success: false,
+                error: "Session already active"
+            })
+        }
+        const attendanceStartData = AttendanceStartSchema.parse(req.body)
+
+        const token = req.headers.authorization;
+        const payload = decodeAccessToken(token)
+
+        if (!payload || payload.role === 'student') {
+            return res.status(403).json({
+                success: false,
+                error: "Unauthorized access",
+            })
+        }
+
+        var _class = await Class.findById(attendanceStartData.classId)
+
+        if (_class) {
+            if (_class.teacherId._id.toString() === payload._id) {
+                const date = new Date().toISOString()
+                activeSession = {
+                    classId: req.body.classId,
+                    startedAt: date,
+                    attendance: {}
+                };
+
+                return res.status(200).json({
+                    "success": true,
+                    "data": {
+                        "classId": attendanceStartData.classId,
+                        "startedAt": date
+                    }
+                })
+            }
+            else {
+                res.status(403).json({ success: false, error: "Unauthorized access" })
+            }
+
+        }
+        else {
+            res.status(404).json({ success: false, error: "Class not found" })
+        }
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json()
     }
 })
 
@@ -109,4 +596,17 @@ const SignupSchema = z.object({
     email: z.email(),
     password: z.string().min(6),
     role: RoleEnum
+})
+
+const LoginSchema = z.object({
+    email: z.email(),
+    password: z.string().min(6),
+})
+
+const ClassSchema = z.object({
+    className: z.string(),
+})
+
+const AttendanceStartSchema = z.object({
+    classId: z.string(),
 })
